0ef62b1b15cb704250470cfd2d66d319ee780d99
Seu Nome <seu.email@example.com>
2025-01-20 14:57:54 -0300

    Fix directory navigation for mirror push and update tag message to skip CI

--- scripts/reset-tags.sh
+++ scripts/reset-tags.sh
@@ -6,5 +6,5 @@ git tag -l | xargs git tag -d
 # Delete remote tags
 git ls-remote --tags origin | awk '{print $2}' | grep -v '{}' | sed 's#refs/tags/##' | xargs -I {} git push origin :refs/tags/{}
 
-git tag v0.0.0 -m "WIP"
+git tag v0.0.0 -m "WIP [skip ci]"
 git push origin v0.0.0
