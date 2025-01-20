ce6dbfe9cf516e21a0da7ff9c60488d20185489a
Seu Nome <seu.email@example.com>
2025-01-20 06:11:21 -0300

    Replace delete-tags.sh with reset-tags.sh for improved tag management functionality

--- scripts/reset-tags.sh
+++ scripts/reset-tags.sh
@@ -5,3 +5,6 @@ git tag -l | xargs git tag -d
 
 # Delete remote tags
 git ls-remote --tags origin | awk '{print $2}' | grep -v '{}' | sed 's#refs/tags/##' | xargs -I {} git push origin :refs/tags/{}
+
+git tag v0.0.0 -m "WIP"
+git push origin v0.0.0
