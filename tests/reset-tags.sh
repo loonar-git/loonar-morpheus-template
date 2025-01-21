8882cd4aa94a3589e51affd7b0d5fe2d56838e57
Seu Nome <seu.email@example.com>
2025-01-21 07:47:34 -0300

    Update CI workflow to ignore tag pushes and enhance reset-tags.sh to create a WIP tag

--- tests/reset-tags.sh
+++ tests/reset-tags.sh
@@ -6,6 +6,9 @@ git tag -l | xargs git tag -d
 # Delete remote tags
 git ls-remote --tags origin | awk '{print $2}' | grep -v '{}' | sed 's#refs/tags/##' | xargs -I {} git push origin :refs/tags/{}
 
+git tag v0.0.0 -m "WIP [skip ci]"
+git push origin v0.0.0
+
 # Função para verificar se há workflows em execução
 check_workflows() {
   local owner="loonar-git"
