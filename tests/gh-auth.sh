3ec72c8f5f140276c9ace32602f35189ee8cd290
Seu Nome <seu.email@example.com>
2025-01-21 05:44:01 -0300

    Enhance gh-auth.sh to include option for YOUR PAT and improve user prompts

--- tests/gh-auth.sh
+++ tests/gh-auth.sh
@@ -11,7 +11,10 @@ fi
 
 # Loop para perguntar ao usuário até que uma opção válida seja fornecida
 while true; do
-  echo "Deseja conectar com a SOURCE ou DESTINATION PAT? (Digite '1' para SOURCE ou '2' para DESTINATION)"
+  echo "Options PAT:"
+  echo "[1] SOURCE"
+  echo "[2] DESTINATION"
+  echo "[3] YOUR"
   read -r PAT_TYPE
 
   if [ "$PAT_TYPE" == "1" ]; then
@@ -20,18 +23,21 @@ while true; do
   elif [ "$PAT_TYPE" == "2" ]; then
     GH_PAT=$DESTINATION_PAT
     break
+  elif [ "$PAT_TYPE" == "3" ]; then
+    GH_PAT=$YOUR_PAT
+    break
   else
-    echo "Opção inválida! Por favor, digite '1' ou '2'."
+    echo "Invalid option"
   fi
 done
 
 # Verificar se GH_PAT foi definido corretamente
 if [ -z "$GH_PAT" ]; then
-  echo "Erro: GH_PAT não foi definido corretamente."
+  echo "Error: GH_PAT has not been defined correctly."
   exit 1
 fi
 
 # Usar o token para autenticação
 echo "$GH_PAT" | gh auth login --with-token
 
-echo "GITHIB_PAT definido com sucesso."
+echo "Githib_pat defined successfully."
