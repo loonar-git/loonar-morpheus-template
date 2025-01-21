6450a327df4d80f1edb210f869a046dc8190b12a
Seu Nome <seu.email@example.com>
2025-01-21 06:25:59 -0300

    Update release.yml to consistently use GH_TOKEN environment variable and enhance reset-tags.sh to create a pre-release with the WIP tag

--- tests/reset-tags.sh
+++ tests/reset-tags.sh
@@ -6,5 +6,8 @@ git tag -l | xargs git tag -d
 # Delete remote tags
 git ls-remote --tags origin | awk '{print $2}' | grep -v '{}' | sed 's#refs/tags/##' | xargs -I {} git push origin :refs/tags/{}
 
-git tag v0.0.0 -m "WIP [skip ci]"
+git tag v0.0.0 -m "WIP"
 git push origin v0.0.0
+
+# Criar um novo release com o nome WIP para a tag v0.0.0 como pre-release
+gh release create v0.0.0 --generate-notes --prerelease
